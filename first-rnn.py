7edd74de48745b38f674bc37bb440f616b14ff7f
Connor Frazier <cfraz814@mail.endicott.edu>
2019-11-13 14:45:51 -0500

    Data Pipeline rough draft

--- first-rnn.py
+++ first-rnn.py
@@ -8,6 +8,9 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+import os
+import pandas as pd
+
 #Quandl
 import quandl
 
@@ -15,11 +18,10 @@ import quandl
 quandl.ApiConfig.api_key = "c41SJX7-N-p3yWF2Ksmk"
 
 data = quandl.get_table('SHARADAR/SF1', ticker='AAPL', dimension='ARQ', calendardate={'gte':'2013-09-01','lte':'2020-01-10'}, paginate=True)
+print(type(data))
 
-target = data.pop('workingcapital')
-
-#Removing emtpty data columns
-data = data.select_dtypes(exclude=['object'])
+# target = data.pop('workingcapital')
+# print(type(target))
 
 #cleaning out timestamps
 data.pop('calendardate')
@@ -27,9 +29,49 @@ data.pop('datekey')
 data.pop('reportperiod')
 data.pop('lastupdated')
 
-#creating tensor with features and target values
-#This needs to change to the time series set up
-dataset = tf.data.Dataset.from_tensor_slices((data.values, target.values))
+TRAIN_SPLIT = 23
 
-for feat, targ in dataset.take(5):
-  print ('Features: {}, Target: {}'.format(feat, targ))
+#Removing emtpty data columns
+data = data.select_dtypes(exclude=['object'])
+#Normalizing
+data_mean = data[:TRAIN_SPLIT].mean(axis=0)
+data_std = data[:TRAIN_SPLIT].std(axis=0)
+
+data = (data-data_mean)/data_std
+
+#Creating data points and labels
+train_data = []
+train_labels = []
+
+
+for i in range(21):
+	train_data.append(data.loc[i:i+3, :].values)
+	train_labels.append(data.loc[i+4, 'accoci':'assetsnc'].values)
+
+train_data = np.array(train_data)
+train_labels = np.array(train_labels)
+
+#creating tensor with features and target values
+dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+
+for feat, targ in dataset.take(2):
+	print ('Features: {}, Target: {}'.format(feat, targ))
+
+dataset = dataset.cache().shuffle(24).batch(4).repeat()	
+
+# for feat, targ in dataset.take(2):
+# 	print ('Features: {}, Target: {}'.format(feat, targ))
+
+simple_lstm_model = tf.keras.models.Sequential([
+    tf.keras.layers.LSTM(8, input_shape=train_data.shape[-2:]),
+    tf.keras.layers.Dense(1)
+])
+
+
+simple_lstm_model.compile(optimizer='adam', loss='mae')
+
+for x, y in dataset.take(1):
+    print(simple_lstm_model.predict(x).shape)
+
+
+
