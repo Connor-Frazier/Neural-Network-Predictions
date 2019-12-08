fd419c5c6ad5af83444ba6f4346b94345a3128f8
cfraz814 <cfraz814@mail.endicott.edu>
2019-12-08 12:40:53 -0500

    Final Code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MLFall2019
+Shagun Bhardwaj, Camil Blanchet and Connor Frazier
+
+pip install tensorflow
+pip install keras
+pip install quandl
+
+The entire testing of all models across all company datasets is run by one script "full-test.py"
+
+run python full-test.py
+
+Then look in your current directory to find the sse_results.csv
--- full-test.py
+++ full-test.py
@@ -4,19 +4,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 from tensorflow import keras
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
-
-import os
-import pandas as pd
-
-import time
-
-import csv
-
 #Quandl
 import quandl
+
+#Helper libraries
+import numpy as np
+import os
+import pandas as pd
+import time
+import csv
 
 def getModel(model_name):
 	print("Running " + model_name)
@@ -59,22 +55,20 @@ def getModel(model_name):
             keras.layers.Dense(4)
             ])	
 
-
-
-#Getting data
+#Set up
+quandl.ApiConfig.api_key = "c41SJX7-N-p3yWF2Ksmk"
+tickers = ['AAPL', 'ATVI', 'ATHN', 'MSFT', 'ADBE', 'ORCL', 'CRM', 'WDAY', 'ACN', 'TWTR', 'CNQR', 'PEGA', 'AZPN', 'BYI']
+models = ["gru1", "gru2", "gru3", "lstm1", "lstm2", "lstm3"]
 quandl.ApiConfig.api_key = "c41SJX7-N-p3yWF2Ksmk"
 
-tickers = ['AAPL', 'ATVI', 'ATHN', 'MSFT', 'ADBE', 'ORCL', 'CRM', 'WDAY', 'ACN', 'TWTR', 'CNQR', 'PEGA', 'AZPN', 'BYI']
-
-models = ["gru1", "gru2", "gru3", "lstm1", "lstm2", "lstm3"]
-# models = ["gru1"] this is used for script testing
+#Setting up results structure
 results = {}
 for modelName in models:
 	results[modelName] = {}
 
-#Getting data
-quandl.ApiConfig.api_key = "c41SJX7-N-p3yWF2Ksmk"
+#Main testing loop
 for ticker in tickers:
+	#Get data
 	data = quandl.get_table('SHARADAR/SF1',
                          dimension = "ARQ",
                          qopts={"columns":['datekey','revenueusd','assets','capex','debt','ebit','equityusd','ev','fcf','inventory','marketcap','netinc','taxexp','workingcapital']},
@@ -102,27 +96,27 @@ for ticker in tickers:
 	data_std = data[:TRAIN_SPLIT].std(axis=0)
 	data = (data-data_mean)/data_std
 
-	#Setting parameters and creating data points and labels
+	#Creating data points and labels for training
 	train_data = []
 	train_labels = []
 
-	#Data points at the moment consist of a history of 4 qvalue data points and the 5th as the target
+	#Training data points contain a history of 4 original data points and the 5th as the target
 	for i in range(TRAIN_SPLIT):
 		train_data.append(data.iloc[i:i+steps, :].values)
-		#'accoci':'assetsnc' are example columns, will be an array for the 4
 		train_labels.append(data.loc[i+steps, predictions].values)
 	
 	train_data = np.array(train_data)
 	train_labels = np.array(train_labels)
 
-	#Fix feature coutn in case data is different than expected
+	#Fix feature count in case data is different than expected
 	featuresCount = len(train_data[1][0]);
 
 
-	#Setting up test data
+	#Creating data points and labels for testing
 	test_data = []
 	test_labels = []
 
+	#Testing data points contain a history of 4 original data points and the 5th as the target
 	for j in range(TEST_SPLIT):
 		temp = TRAIN_SPLIT - 1 - steps + j
 		test_data.append(data.iloc[temp:temp + steps, :].values)
@@ -131,16 +125,16 @@ for ticker in tickers:
 	test_data = np.array(test_data)
 	test_labels = np.array(test_labels)
 
+	#Train and test all models on this dataset
 	for modelName in models:
 		#Get the model to test
 		model = getModel(modelName)
 
 		#Compile and train
-		# model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 		model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(0.001), metrics=['mse'])
 		model.fit(train_data, train_labels, epochs=epochs)
 		
-		# predicting the test data points
+		#Predicting the test data points
 		print("Testing")
 		results[modelName][ticker] = {}
 		sse = 0
